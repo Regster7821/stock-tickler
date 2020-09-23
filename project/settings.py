2c9308fec87fb798be9d9285843dc28ebde9c4df
randyrodgers <randy.e.rodgers@gmail.com>
2020-09-22 20:46:44 -0500

    added React to Django

--- project/settings.py
+++ project/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join( BASE_DIR, 'frontend/build' ),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join( BASE_DIR, 'frontend/build/static' ),
+]
